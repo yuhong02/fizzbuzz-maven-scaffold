02aff9db993036d2d941c22e2b2f4b6cd76e74b0
yuhong02 <myh0510@hotmail.com>
2019-12-28 13:52:55 +0800

    update

--- src/test/java/FizzBuzzIITest.java
+++ src/test/java/FizzBuzzIITest.java
@@ -5,14 +5,18 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.lang.Exception;
+
+import org.junit.rules.ExpectedException;
 public class FizzBuzzIITest {
     @Test
     public void FizzBuzz3(){
     	int number =3;
-    	List<String> result = FizzBuzz.fizzBuzz(number);
+    	List<String> result = FizzBuzzII.fizzBuzz(number);
     	List<String> result1 = new ArrayList<>();
     	result1.add("1");
     	result1.add("2");
@@ -23,7 +27,7 @@ public class FizzBuzzIITest {
     @Test
     public void FizzBuzz4(){
     	int number =4;
-    	List<String> result = FizzBuzz.fizzBuzz(number);
+    	List<String> result = FizzBuzzII.fizzBuzz(number);
     	List<String> result1 = new ArrayList<>();
     	result1.add("1");
     	result1.add("2");
@@ -34,7 +38,7 @@ public class FizzBuzzIITest {
     @Test
     public void FizzBuzz6(){
     	int number =6;
-    	List<String> result = FizzBuzz.fizzBuzz(number);
+    	List<String> result = FizzBuzzII.fizzBuzz(number);
     	List<String> result1 = new ArrayList<>();
     	result1.add("1");
     	result1.add("2");
@@ -48,7 +52,7 @@ public class FizzBuzzIITest {
     @Test
     public void FizzBuzz5(){
     	int number =5;
-    	List<String> result = FizzBuzz.fizzBuzz(number);
+    	List<String> result = FizzBuzzII.fizzBuzz(number);
     	List<String> result1 = new ArrayList<>();
     	result1.add("1");
     	result1.add("2");
@@ -60,7 +64,7 @@ public class FizzBuzzIITest {
     @Test
     public void FizzBuzz10(){
     	int number =10;
-    	List<String> result = FizzBuzz.fizzBuzz(number);
+    	List<String> result = FizzBuzzII.fizzBuzz(number);
     	List<String> result1 = new ArrayList<>();
     	result1.add("1");
     	result1.add("2");
@@ -68,7 +72,7 @@ public class FizzBuzzIITest {
     	result1.add("4");
     	result1.add("Buzz");
     	result1.add("Fizz");
-    	result1.add("7");
+    	result1.add("Whizz");
     	result1.add("8");
     	result1.add("Fizz");
     	result1.add("Buzz");
@@ -78,7 +82,7 @@ public class FizzBuzzIITest {
     @Test
     public void FizzBuzz15(){
     	int number =15;
-    	List<String> result = FizzBuzz.fizzBuzz(number);
+    	List<String> result = FizzBuzzII.fizzBuzz(number);
     	List<String> result1 = new ArrayList<>();
     	result1.add("1");
     	result1.add("2");
@@ -86,22 +90,22 @@ public class FizzBuzzIITest {
     	result1.add("4");
     	result1.add("Buzz");
     	result1.add("Fizz");
-    	result1.add("7");
+    	result1.add("Whizz");
     	result1.add("8");
     	result1.add("Fizz");
     	result1.add("Buzz");
     	result1.add("11");
     	result1.add("Fizz");
     	result1.add("13");
-    	result1.add("14");
+    	result1.add("Whizz");
     	result1.add("FizzBuzz");
-    	assertThat(result).isEqualTo(result);
+    	assertThat(result).isEqualTo(result1);
     }
     
     @Test
     public void FizzBuzz30(){
     	int number =30;
-    	List<String> result = FizzBuzz.fizzBuzz(number);
+    	List<String> result = FizzBuzzII.fizzBuzz(number);
     	List<String> result1 = new ArrayList<>();
     	result1.add("1");
     	result1.add("2");
@@ -109,31 +113,39 @@ public class FizzBuzzIITest {
     	result1.add("4");
     	result1.add("Buzz");
     	result1.add("Fizz");
-    	result1.add("7");
+    	result1.add("Whizz");
     	result1.add("8");
     	result1.add("Fizz");
     	result1.add("Buzz");
     	result1.add("11");
     	result1.add("Fizz");
     	result1.add("13");
-    	result1.add("14");
+    	result1.add("Whizz");
     	result1.add("FizzBuzz");
-    	result1.add("Fizz");
+    	result1.add("16");
     	result1.add("17");
     	result1.add("Fizz");
     	result1.add("19");
     	result1.add("Buzz");
-    	result1.add("Fizz");
+    	result1.add("FizzWhizz");
     	result1.add("22");
     	result1.add("23");
     	result1.add("Fizz");
     	result1.add("Buzz");
     	result1.add("26");
     	result1.add("Fizz");
-    	result1.add("28");
+    	result1.add("Whizz");
     	result1.add("29");
     	result1.add("FizzBuzz");
-    	assertThat(result).isEqualTo(result);
+    	assertThat(result).isEqualTo(result1);
+    }    	
+    @Rule 
+    public ExpectedException expectedException = ExpectedException.none();
+    @Test
+    public void exception(){
+    	expectedException.expect(IllegalArgumentException.class);
+    	expectedException.expectMessage("Invalid input");
+    	int number =-1;
+    	FizzBuzzII.fizzBuzz(number);
     }
-
 }
