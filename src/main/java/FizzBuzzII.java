6c81764fc9e5a47f61e78be428f5e85916e72888
yuhong02 <myh0510@hotmail.com>
2019-12-28 13:52:15 +0800

    update

--- src/main/java/FizzBuzzII.java
+++ src/main/java/FizzBuzzII.java
@@ -3,9 +3,12 @@ import java.util.*;
 public class FizzBuzzII{
 	public static List<String> fizzBuzz(int n) {
 		List<String> res = new ArrayList<>();
+		if(n<=0){
+			throw new IllegalArgumentException("Invalid input");
+		}
 		for (int i = 1;i <= n;i ++){
 		String tmp = "";
-		if (i % 3 != 0 && i % 5 != 0){
+		if (i % 3 != 0 && i % 5 != 0 && i % 7 != 0){
 		 tmp += i;
 		 res.add(tmp);
 		tmp = "";
@@ -16,6 +19,9 @@ public class FizzBuzzII{
 		}
 		if (i % 5 == 0){
 		tmp += "Buzz";
+		}
+		if (i % 7 == 0){
+		tmp += "Whizz";
 		}
 		res.add(tmp);
 		tmp = "";
